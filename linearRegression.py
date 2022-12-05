3edc6531204af81945eff28b5dd010597b540e20
danielwoo-work <danielwoo.work@gmail.com>
2022-12-05 12:17:29 +0800

    Create linearRegression.py

---
+++ linearRegression.py
@@ -0,0 +1,14 @@
+# Import the relevant libraries
+
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+
+# Import kstest function from Scipy, and Linear Regression function from SKLearn
+
+from scipy.stats import kstest
+from sklearn.linear_model import LinearRegression
+
+
+
+
